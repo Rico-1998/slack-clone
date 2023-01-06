9d3883a7ee803159291bb3b67b66fb3d38504756
Phil Schmucker <p.schmuck.er@web.de>
2023-01-06 20:01:05 +0100

    basic framework

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DialogSuccessMessageComponent } from './dialog-components/dialog-success-message/dialog-success-message.component';
+import { HomeComponent } from './home/home.component';
 import { StartScreenComponent } from './start-screen/start-screen.component';
 
 const routes: Routes = [
   { path: '', component: StartScreenComponent },
   { path: 'success', component: DialogSuccessMessageComponent },
+  { path: 'home', component: HomeComponent },
 ];
 
 @NgModule({
---
+++ src/app/nav-tree/nav-tree.component.ts
@@ -0,0 +1,64 @@
+import { FlatTreeControl, NestedTreeControl } from '@angular/cdk/tree';
+import { Component, OnInit } from '@angular/core';
+import { MatTreeFlatDataSource, MatTreeFlattener, MatTreeNestedDataSource } from '@angular/material/tree';
+
+interface chatNode {
+  name: string;
+  children?: chatNode[];
+}
+
+const TREE_DATA: chatNode[] = [
+  {
+    name: 'Channels',
+    children: [{name: '#allgemein'}, {name: '#random'}, {name: '#Angular'}],
+  },
+  {
+    name: 'Chats',
+    children: [{name: 'Rico'}, {name: 'Tobias'}, {name: 'Viktor'}, {name: 'Phil'}],
+  },
+];
+
+interface ExampleFlatNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+}
+
+@Component({
+  selector: 'app-nav-tree',
+  templateUrl: './nav-tree.component.html',
+  styleUrls: ['./nav-tree.component.scss']
+})
+export class NavTreeComponent implements OnInit {
+  private _transformer = (node: chatNode, level: number) => {
+    return {
+      expandable: !!node.children && node.children.length > 0,
+      name: node.name,
+      level: level,
+    };
+  };
+
+  treeControl = new FlatTreeControl<ExampleFlatNode>(
+    node => node.level,
+    node => node.expandable,
+  );
+
+  treeFlattener = new MatTreeFlattener(
+    this._transformer,
+    node => node.level,
+    node => node.expandable,
+    node => node.children,
+  );
+
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+  constructor() {
+    this.dataSource.data = TREE_DATA;
+  }
+
+  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
+
+  ngOnInit(): void {
+  }
+
+}
