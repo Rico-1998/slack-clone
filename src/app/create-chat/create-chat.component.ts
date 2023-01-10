e2b59976bd631b60a07ecbb75432c5df39cc0bc8
Rico-1998 <rico.dg40@gmail.com>
2023-01-10 09:25:46 +0100

    changes

--- src/app/create-chat/create-chat.component.ts
+++ src/app/create-chat/create-chat.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, HostListener } from '@angular/core';
+import { Firestore, getFirestore, onSnapshot } from '@angular/fire/firestore';
+import { collection } from '@firebase/firestore';
 import { DrawerTogglerService } from '../services/drawer-toggler.service';
+import { UserService } from '../services/user.service';
+
 
 @Component({
   selector: 'app-create-chat',
@@ -13,14 +17,25 @@ export class CreateChatComponent implements OnInit {
     "date": '09.01.2022',
     "message": 'Du: Das ist eine Testnachricht!',
   }
+  searchedUSers = [];
 
-  constructor(public toggler: DrawerTogglerService) { }
+  constructor(public toggler: DrawerTogglerService, private firestore: Firestore, private userService: UserService) { }
 
   ngOnInit(): void {
   }
 
   @HostListener('window:resize', ['$event'])
-    onResize(event) {
-    this.viewportWidth = event.target.innerWidth > 600 ? false : true;    
-  } 
+  onResize(event) {
+    this.viewportWidth = event.target.innerWidth > 600 ? false : true;
+  }
+
+  searchUser(event: any) {
+    this.userService.users.forEach((user, index) => {
+      let users: string = user.userName.toLowerCase();
+      if (users.startsWith(event.key.toLowerCase() as string)) {
+        this.searchedUSers.push(user.userName);
+      }
+    });
+  }
+
 }
