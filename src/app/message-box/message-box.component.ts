6bae09870406eec59883a58362bef6979aa7b4c8
v1cke <v1cke1982@gmail.com>
2023-01-10 22:52:52 +0100

    update channels component

--- src/app/message-box/message-box.component.ts
+++ src/app/message-box/message-box.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnChanges, OnInit } from '@angular/core';
 import { Firestore } from '@angular/fire/firestore';
 import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { doc, getDoc, getFirestore } from '@firebase/firestore';
 import 'quill-emoji/dist/quill-emoji.js';
+import { Message } from 'src/modules/messages.class';
 
 
 
@@ -29,10 +32,15 @@ export class MessageBoxComponent implements OnInit {
   };
 
   messageForm: FormGroup;
-  text: any = '';
+  message = new Message;
+  text : string; '';
+  messageID: string = '';
+  channelName: any = '';
+  db: any = getFirestore();
 
 
-  constructor(public firestore:Firestore) {
+  constructor(public firestore:Firestore,
+    private route: ActivatedRoute) {
     this.messageForm = new FormGroup({
       'msgEditor': new FormControl()
     })
@@ -42,13 +50,20 @@ export class MessageBoxComponent implements OnInit {
 
   }
 
-  sendMessage() {
-
+  async sendMessage() {
+    this.route.params.subscribe((params) => {
+      this.messageID = params['id'];
+      let document = doc(this.db, 'messages', this.messageID);
+      getDoc(document)
+        .then((doc) => {
+          console.log(doc.data());
+        })
+    })
   }
 
   checkEditor(event) {
     console.log(event.event);
-    console.log(this.text);
+    console.log(this.message);
 
   }
 
