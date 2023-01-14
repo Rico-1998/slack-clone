560260a7ea4d128cdc52ad24bda5c1f6ed69f146
v1cke <v1cke1982@gmail.com>
2023-01-14 20:11:27 +0100

    cancel firestoreservice

--- src/app/channels/channels.component.ts
+++ src/app/channels/channels.component.ts
@@ -4,8 +4,7 @@ import { Channel } from 'src/modules/channels.class';
 import { timestamp } from 'rxjs';
 import { doc, Firestore, getDoc } from '@angular/fire/firestore';
 import { collection, getFirestore, onSnapshot } from '@firebase/firestore';
-import { FirestoreService } from '../services/firestore.service';
-import { UserService } from '../services/user.service';
+// import { FirestoreService } from '../services/firestore.service';
 
 
 
@@ -17,41 +16,22 @@ import { UserService } from '../services/user.service';
 export class ChannelsComponent implements OnInit {
   channelId: string;
   channel: Channel;
-  userName: any;
   date: Date;
   db: any = getFirestore();
   currentChannel: any = '';
 
   constructor(
-    public firestoreService: FirestoreService,
-    public user: UserService,
     private route: ActivatedRoute,
-    private firestore: Firestore
   ) { }
 
   ngOnInit(): void {
-    // this.channel = new Channel;
-    // this.route.params.subscribe((params) => {
-    //   this.channelId = params['id'];
-    //   this.angularFirestore
-    //     .collection('channels')
-    //     .doc(this.channelId)
-    //     .valueChanges()
-    //     .subscribe((channel: any) => {
-    //       this.channel.channelName = channel.channelName;
-    //       this.channel.channelDescription = channel.channelDescription;
-    //       this.channel.created = channel.created;
-    //       this.channel.messages = channel.messages;
-    //     });
-    // })
-
     this.route.params.subscribe((params) => {
       this.channelId = params['id'];
       let document = doc(this.db, 'channels', this.channelId);
       getDoc(document)
         .then((doc) => {
-          this.firestoreService.channelID = this.channelId;
-          this.firestoreService.currentChannel = doc.data();
+          this.currentChannel = doc.data();
+          console.log(this.currentChannel );
         })
     })
   }
--- src/app/message-box/message-box.component.ts
+++ src/app/message-box/message-box.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnChanges, OnInit } from '@angular/core';
-import { Firestore } from '@angular/fire/firestore';
+import { Component, OnChanges, OnInit, ViewChild } from '@angular/core';
+import { addDoc, collection, Firestore, onSnapshot } from '@angular/fire/firestore';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { doc, getDoc, getFirestore } from '@firebase/firestore';
+import { QuillEditorComponent } from 'ngx-quill';
 import 'quill-emoji/dist/quill-emoji.js';
 import { Message } from 'src/modules/messages.class';
-import { FirestoreService } from '../services/firestore.service';
+import { ChannelsComponent } from '../channels/channels.component';
+import { UserService } from '../services/user.service';
 
 
 
@@ -15,7 +17,8 @@ import { FirestoreService } from '../services/firestore.service';
   styleUrls: ['./message-box.component.scss']
 })
 export class MessageBoxComponent implements OnInit {
-  messageText: string = '';
+  @ViewChild('messageInput')
+  messageInput: QuillEditorComponent;
 
   modules = {
     'emoji-shortname': true,
@@ -32,38 +35,35 @@ export class MessageBoxComponent implements OnInit {
     ]
   };
 
+  userName: string;
+  allMessages: any = [];
   messageForm: FormGroup;
   message = new Message;
-  text : string; '';
+  text: string; '';
   messageID: string = '';
-  channelName: any = '';
   db: any = getFirestore();
+  currentChannel: any;
 
 
   constructor(
-    public firestore:Firestore,
-    public firestoreService: FirestoreService,
-    private route: ActivatedRoute) {
+    public firestore: Firestore,
+    public userService: UserService,
+    private route: ActivatedRoute,
+    public channel: ChannelsComponent,
+  ) {
     this.messageForm = new FormGroup({
       // 'msgEditor': new FormControl()
-      msgEditor : new FormControl()
+      msgEditor: new FormControl()
     })
   }
-
+  
   ngOnInit(): void {
+    console.log(this.userService.currentUser$);
 
+        console.log(this.channel.currentChannel);
   }
 
-  async sendMessage() {
-    this.route.params.subscribe((params) => {
-      this.messageID = params['id'];
-      let document = doc(this.db, 'messages', this.messageID);
-      getDoc(document)
-        .then((doc) => {
-          console.log(doc.data());
-        })
-    })
-  }
+
 
   checkEditor(event) {
     // console.log(event.event);
@@ -71,11 +71,16 @@ export class MessageBoxComponent implements OnInit {
 
   }
 
-  onSubmit(){
-    this.firestoreService.messageInput = this.messageForm.value.msgEditor;
-    this.firestoreService.postMessage();
-    console.log(this.firestoreService.messageInput);
+  onSubmit() {
+    this.message = this.messageForm.value.msgEditor;
+    // this.messageInput.quillEditor.getContents([]);
+
+    let ref = collection(this.db, 'channels', this.channel.channelId as string, 'messages');
+    onSnapshot(ref, (snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        this.allMessages.push({ ...(doc.data() as object), id: doc.id });
+        console.log(this.allMessages);
+      })
+    })
   }
-
-
 }
