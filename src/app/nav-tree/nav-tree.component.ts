77001fb94d96f61e4e7f9ef9309b0f195313711d
Rico-1998 <rico.dg40@gmail.com>
2023-01-16 21:24:09 +0100

    prototype for rendering chat section

--- src/app/nav-tree/nav-tree.component.ts
+++ src/app/nav-tree/nav-tree.component.ts
@@ -19,6 +19,7 @@ export class NavTreeComponent implements OnInit {
   db = getFirestore();
   currentUser = JSON.parse(localStorage.getItem('user'));
   currentUserChats = query(collection(this.db, 'chats'), where('userIds', 'array-contains', this.currentUser.uid))
+  otherChatMembers: any = [];
   channels: any = [];
   chats: any[] = ['Tobias', 'Rico', 'Phil', 'Viktor'];
 
@@ -28,22 +29,41 @@ export class NavTreeComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    onSnapshot(this.currentUserChats, async (snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        this.otherChatMembers.push(((doc.data()['userIds'].filter(a => a != this.currentUser.uid))))
+      })
+      for (let i = 0; i < this.otherChatMembers.length; i++) {
+        const actualMember = this.otherChatMembers[i][0];
+        let q2 = query(collection(this.db, 'users'), where('id', '==', actualMember))
+        let userDocs = await getDocs(q2)
+          .then((docData) => {
+            console.log('das sind die nutzer daten aus users', docData.docs.map(a => a.data()));
+          })
+      }
+    })
+
+    console.log('das ist otherMembers', this.otherChatMembers);
+    //DIESE FUNKTION KLAPPT SO DASS MAN SICH NEN PRIVAT CHAT HOLEN KANN ABER WENN MEHR ALS 2 LEUTE IM CHAT SIND
+    // DANN FILTERT ER TROTZDEM ALLE NUTZER RAUS UND NICHT QUASI DIE GRUPPEN
+
+
+
 
     onSnapshot(collection(this.db, 'channels'), (snapshot) => {
       this.channels = [];
       snapshot.docs.forEach((doc) => {
-        this.channels.push(({ ...(doc.data() as object), id: doc.id }));        
+        this.channels.push(({ ...(doc.data() as object), id: doc.id }));
       })
     })
-
-    getDocs(this.currentUserChats)
-      .then((docData) => {
-        this.chats = [];
-        docData.docs.forEach((user) => {
-          this.chats.push(({ ...(user.data() as object), id: user.id }));
-          console.log('chats',this.chats);
-        });
-      })
+    // getDocs(this.currentUserChats)
+    //   .then((docData) => {
+    //     this.chats = [];
+    //     docData.docs.forEach((user) => {
+    //       this.chats.push(({ ...(user.data() as object), id: user.id }));
+    //       console.log('chats',this.chats);
+    //     });
+    //   })
   }
 
   openDialogAddChannel() {
