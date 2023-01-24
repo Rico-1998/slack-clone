d90d9566c27f32c5f7cc90a7883273a1a7098322
Tobias Fernkorn <fernsikos@gmail.com>
2023-01-24 16:25:50 +0700

    started with responsive home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { Firestore } from '@angular/fire/firestore';
 import { AuthService } from '../services/auth.service';
 import { DrawerTogglerService } from '../services/drawer-toggler.service';
@@ -12,12 +12,24 @@ import { UserService } from '../services/user.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public type: any = 'side';
+  public open: boolean = true;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    if (window.innerWidth < 700) {
+      this.type = 'over';
+      this.open = false;
+    } else {
+      this.type = 'side';
+      this.open = true;
+    }
+  }
 
   constructor(public authService: AuthService, private firestore: Firestore, private userService: UserService, public toggler: DrawerTogglerService) { }
 
   ngOnInit(): void {
     this.userService.getData();
-
   }
 
 }
