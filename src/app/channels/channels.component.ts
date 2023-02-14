537bdbfb6dd50f9f579ec63db2e119e7bf1e41fb
Tobias Fernkorn <fernsikos@gmail.com>
2023-02-14 19:52:50 +0700

    small highlight bugfix

--- src/app/channels/channels.component.ts
+++ src/app/channels/channels.component.ts
@@ -38,6 +38,10 @@ export class ChannelsComponent implements OnInit {
     public deleteDialogService: DeleteDialogService,
   ) {
     route.params.subscribe((channelRoomId) => {
+      if (this.channelService.channelId) {
+        console.log(this.channelService.channelId,'#1')
+       this.channelService.updateLastVisitTimestamp();
+      }
       this.channelService.channelId = channelRoomId['id'];
     });
   }
@@ -49,9 +53,11 @@ export class ChannelsComponent implements OnInit {
       this.channelService.destroy();
         //Checkes if we alredy visited a channel and updates the lastVisitTimestamp
       if (this.channelService.channelId) {
+        console.log(this.channelService.channelId,'#2')
         await this.channelService.updateLastVisitTimestamp();
       }
       this.channelService.getChannelRoom(channelRoomId);
+      console.log(this.channelService.channelId,'#3')
     })
     this.channelService.updateLastVisitTimestamp()
     this.userService.channelEditor = true;
